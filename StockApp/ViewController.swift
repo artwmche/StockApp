6f4de8cd0899ca1c6584ec6ca8b90ab2375a8e6b
Arthur Cheung <a.cheung2@sympatico.ca>
2018-11-27 10:12:05 -0500

    Initial Commit

---
+++ StockApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by Arthur Cheung on 2018-11-27.
+//  Copyright © 2018 Arthur Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
